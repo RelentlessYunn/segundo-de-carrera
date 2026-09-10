25ee62b7a7817bc62b2f3e094bec0c62fbec0f4b
Shengyu <shengyu.chan7@gmail.com>
2026-09-10 14:28:53 +0200

    Change API_KEY in config.js
    Updated API_KEY in the configuration object.

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
    config.js está en .gitignore: nunca se sube al repositorio. */
 window.CONFIG = {
   BIN_ID:  "6aa182c1ac6210605ab8c17f",
-  API_KEY: "$2a$10$8xDxttT9ezi3IupFHChPXuK9b29Cl8FdeOCqAZ/WZ4weeGRoYFc1K"
+  API_KEY: "$2a$10$Ege70WJp4UmAy1H1nexh1OZpvmTj/nJZ5iBpcLtnq43vdo2md6Wra"
 };
