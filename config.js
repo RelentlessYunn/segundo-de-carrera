6d4edc53c5d54047b027585a583c1c8ef3012806
Shengyu <shengyu.chan7@gmail.com>
2026-09-10 16:04:47 +0200

    Clean up config.js by removing comments
    Removed instructions for copying and using config.js.

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-/* Copia este archivo como config.js y pon tus credenciales.
-   config.js está en .gitignore: nunca se sube al repositorio. */
 window.CONFIG = {
   BIN_ID:  "6aa182c1ac6210605ab8c17f",
   API_KEY: "$2a$10$Ege70WJp4UmAy1H1nexh1OZpvmTj/nJZ5iBpcLtnq43vdo2md6Wra"
